6b5ce04f22d13a14fa4f22ce218b3db431002dbe
Trip McStocky Legwarmers <daniel.levine4@gmail.com>
2012-04-08 07:15:54 -0400

    another idea

--- vendor/extensions/jolly/app/controllers/admin/states_controller.rb
+++ vendor/extensions/jolly/app/controllers/admin/states_controller.rb
@@ -1,16 +1,21 @@
 class Admin::StatesController < Admin::ResourceController
-  belongs_to :country
+  # belongs_to :country
+  before_filter :load_country
   #before_filter :load_data, :except => [:index]
 
   def index
-    debugger
+    #debugger
     respond_to do |format|
       format.html
-      format.js  { render :partial => 'state_list.html.erb' }
+      format.js  { render :partial => 'state_list.haml' }
     end
   end
 
   protected
+  
+  def load_country
+    @country = Country.find(214)
+  end
   
   def location_after_save
     admin_country_states_url(@country)
