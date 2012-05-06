5adfe0a9c119553ed0c72cf272dfc2c0c2ba0ebf
Trip McStocky Legwarmers <trip@crankapps.com>
2012-05-06 20:01:44 +0200

    updating gmail config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,16 @@ LiveJollyFoods::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'livejollyfoods.com',
+    :user_name            => 'chrisisraw',
+    :password             => 'imissuj9',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+  
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,17 @@ LiveJollyFoods::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'livejollyfoods.com',
+    :user_name            => 'chrisisraw@gmail.com',
+    :password             => 'imissuj9',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+  
+  
 end
